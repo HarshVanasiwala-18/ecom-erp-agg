7c92ee1e3dc756bd9695c3017864ec6ef7e28ac5
Harsh Vanasiwala <81178088+HarshVanasiwala-18@users.noreply.github.com>
2022-01-06 14:34:24 +0530

    Delete ecom-agg/ecom-agg-main directory

--- ecom-agg/ecom-agg-main/components/SupabaseContext.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const SupabaseContext = React.createContext(null);
-
-export { SupabaseContext };
--- ecom-agg/ecom-agg-main/components/index.ts
+++
@@ -1,6 +0,0 @@
-import { Card } from '@supabase/ui';
-import { Layout } from './Layout';
-import { OrderCard } from './OrderCard';
-import { SupabaseContext } from './SupabaseContext';
-
-export { Card, Layout, OrderCard, SupabaseContext };
